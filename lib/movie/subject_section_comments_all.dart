a673ca8710d9e1eda1f69b18b72139bba5117e81
Quan-hao.Lin <quan-hao.lin@sap.com>
2019-10-16 14:09:55 +0800

    sort all comments

--- lib/movie/subject_section_comments_all.dart
+++ lib/movie/subject_section_comments_all.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_douban2/util/client_api.dart';
 import 'package:flutter_douban2/movie/subject_section_comment_template.dart';
+import 'package:flutter_douban2/widget/radio_bar.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:flutter_douban2/util/screen_size.dart';
 
 class SubjectSectionCommentsAll extends StatefulWidget {
   final subjectId;
@@ -12,14 +15,24 @@ class SubjectSectionCommentsAll extends StatefulWidget {
 
 class _SubjectSectionCommentsAllState extends State<SubjectSectionCommentsAll> {
   static const String _loading = "##loading##";
+  var total = '';
   var _start = 0;
   var _count = 20;
   var _done = false;
+  var _sort = 'new_score';
+  var _status = "P";
   var _dataList = <dynamic>[
     {
       "title": _loading,
     }
   ];
+
+  void _refresh() {
+    _start = 0;
+    _done = false;
+    _dataList.removeRange(0, _dataList.length - 1);
+    setState(() {});
+  }
 
   void _retrieveData() async {
     if (_done) {
@@ -30,27 +43,90 @@ class _SubjectSectionCommentsAllState extends State<SubjectSectionCommentsAll> {
       subjectId: this.widget.subjectId,
       start: this._start,
       count: this._count,
+      sort: this._sort,
+      status: this._status,
     );
 
     if (list['comments'].length < this._count) {
       _done = true;
     }
+    this.total = list['total'];
     _dataList.insertAll(_dataList.length - 1, list['comments'].toList());
     _start = _start + list.length;
-    setState(() {});
+    if (mounted) setState(() {});
   }
 
-  @override
-  void initState() {
-    super.initState();
-  }
+  List radios = [
+    {"id": "new_score", "label": '热门'},
+    {"id": "time", "label": '最新'},
+  ];
 
   @override
   Widget build(BuildContext context) {
     return Container(
-      height: 900,
       child: Column(
         children: <Widget>[
+          Row(
+            children: <Widget>[
+              Text(
+                '全部短评',
+                style: TextStyle(fontSize: 24),
+              ),
+              Icon(
+                Icons.help_outline,
+              ),
+            ],
+          ),
+          Row(
+            children: <Widget>[
+              Text(this.total),
+              Expanded(
+                child: Container(),
+              ),
+              RadioBar(
+                radios: radios,
+                onSelectionChange: onSortSelectionChange,
+              ),
+              GestureDetector(
+                onTap: () {
+                  showSelectDialog().then((status) {
+                    if (status != "C" && mounted) {
+                      setState(() {
+                        this._status = status;
+                      });
+
+                      _refresh();
+                    }
+                  });
+                },
+                child: Container(
+                  padding: EdgeInsets.only(
+                    left: ScreenUtil.getInstance().setWidth(ScreenSize.padding),
+                    right:
+                        ScreenUtil.getInstance().setWidth(ScreenSize.padding),
+                  ),
+                  margin: EdgeInsets.all(
+                      ScreenUtil.getInstance().setWidth(ScreenSize.padding)),
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.all(
+                      Radius.circular(14),
+                    ),
+                    border: Border.all(
+                      color: Colors.black,
+                    ),
+                  ),
+                  child: Center(
+                      child: Row(
+                    children: <Widget>[
+                      Text(this._status == "P" ? "看过" : "想看"),
+                      Icon(Icons.keyboard_arrow_down),
+                    ],
+                  )),
+                ),
+              )
+            ],
+          ),
           Expanded(
             child: ListView.separated(
               itemCount: _dataList.length,
@@ -60,7 +136,7 @@ class _SubjectSectionCommentsAllState extends State<SubjectSectionCommentsAll> {
                   return Container();
                 } else {
                   return Container(
-                    child: _builcComment(_dataList[index]),
+                    child: _buildComment(_dataList[index]),
                   );
                 }
               },
@@ -72,7 +148,56 @@ class _SubjectSectionCommentsAllState extends State<SubjectSectionCommentsAll> {
     );
   }
 
-  Widget _builcComment(comment) {
+  Future<String> showSelectDialog() async {
+    // var widget = SelectDialog(this._status);
+    // var result = await showDialog(
+    //   context: context,
+    //   builder: (_) => widget,
+    //   barrierDismissible: false,
+    // );
+    // return result;
+
+    var result = await showDialog(
+        context: context,
+        barrierDismissible: false,
+        builder: (context) {
+          return new SimpleDialog(
+            title: new Text("标记"),
+            children: <Widget>[
+              new SimpleDialogOption(
+                child: Container(
+                  child: new Text("看过"),
+                  color: this._status == 'P' ? Colors.cyan : Colors.transparent,
+                ),
+                onPressed: () {
+                  Navigator.of(context).pop("P");
+                },
+              ),
+              new SimpleDialogOption(
+                child: Container(
+                  child: new Text("想看"),
+                  color: this._status == 'F' ? Colors.cyan : Colors.transparent,
+                ),
+                onPressed: () {
+                  Navigator.of(context).pop("F");
+                },
+              ),
+              new SimpleDialogOption(
+                child: Container(
+                  child: new Text("取消"),
+                  color: this._status == 'C' ? Colors.cyan : Colors.transparent,
+                ),
+                onPressed: () {
+                  Navigator.of(context).pop("C");
+                },
+              ),
+            ],
+          );
+        });
+    return result.toString();
+  }
+
+  Widget _buildComment(comment) {
     return SubjectSectionCommentTemplate(
       authorAvatar: comment['authorAvatar'],
       authorName: comment['authorName'],
@@ -82,6 +207,48 @@ class _SubjectSectionCommentsAllState extends State<SubjectSectionCommentsAll> {
       createdAt: comment['createdAt'],
       content: comment['content'],
       usefufCount: comment['usefufCount'],
+    );
+  }
+
+  void onSortSelectionChange(id) {
+    this._sort = id;
+    _refresh();
+  }
+}
+
+class SelectDialog extends StatelessWidget {
+  final String status;
+  const SelectDialog(this.status, {Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      // color: Colors.red,
+      child: Row(
+        children: <Widget>[
+          RaisedButton(
+            child: Text('看过'),
+            color: this.status == 'P' ? Colors.cyan : Colors.grey,
+            onPressed: () {
+              Navigator.of(context).pop('P');
+            },
+          ),
+          RaisedButton(
+            child: Text('想看'),
+            color: this.status == 'F' ? Colors.cyan : Colors.grey,
+            onPressed: () {
+              Navigator.of(context).pop('F');
+            },
+          ),
+          RaisedButton(
+            child: Text('取消'),
+            color: this.status == 'C' ? Colors.cyan : Colors.grey,
+            onPressed: () {
+              Navigator.of(context).pop('C');
+            },
+          ),
+        ],
+      ),
     );
   }
 }
