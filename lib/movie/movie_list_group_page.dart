3cf5cafb2d09821686e2971b783693272c50bad1
Quan-hao.Lin <quan-hao.lin@sap.com>
2019-10-21 14:07:14 +0800

    add week day info

--- lib/movie/movie_list_group_page.dart
+++ lib/movie/movie_list_group_page.dart
@@ -276,13 +276,44 @@ class _MovieListGroupPageState extends State<MovieListGroupPage> {
   }
 
   String _getPubdate(index) {
-    var pubdates = getSubject(index)['pubdates'];
-    var pubdate = '';
-    pubdates.forEach((date) {
-      if (date.toString().indexOf('中国大陆') != -1) {
-        pubdate = date;
-      }
-    });
-    return pubdate;
+    // var pubdates = getSubject(index)['pubdates'];
+    // var pubdate = '';
+    // pubdates.forEach((date) {
+    //   if (date.toString().indexOf('中国大陆') != -1) {
+    //     pubdate = date;
+    //   }
+    // });
+    // return pubdate;
+    return this._formatDate(getSubject(index)['mainland_pubdate']);
+  }
+
+  String _formatDate(date) {
+    var time = DateTime.parse(date);
+    var day = "";
+    switch (time.weekday) {
+      case 1:
+        day = "星期一";
+        break;
+      case 2:
+        day = "星期二";
+        break;
+      case 3:
+        day = "星期三";
+        break;
+      case 4:
+        day = "星期四";
+        break;
+      case 5:
+        day = "星期五";
+        break;
+      case 6:
+        day = "星期六";
+        break;
+      case 7:
+        day = "星期日";
+        break;
+      default:
+    }
+    return "${time.year}年${time.month}月${time.day}日 $dayÎ";
   }
 }
