6ebf1de2a7e332f844a369f71d416f28d7f34a02
Quan-hao.Lin <quan-hao.lin@sap.com>
2019-10-28 13:09:55 +0800

    2017&2018 top movie&tv

--- lib/movie/movie_rank_years_page.dart
+++ lib/movie/movie_rank_years_page.dart
@@ -27,7 +27,7 @@ class MovieRankList {
   static List<Widget> buildList() {
     List<Widget> list = [];
     list.add(MovieViewSectionHeader("2018"));
-    MovieRankList.list.forEach((f) {
+    MovieRankList.list2018.forEach((f) {
       list.add(MovieRankSectionYear(
         year: '2018',
         type: f['type'],
@@ -37,7 +37,7 @@ class MovieRankList {
     });
 
     list.add(MovieViewSectionHeader("2017"));
-    MovieRankList.list.forEach((f) {
+    MovieRankList.list2017.forEach((f) {
       list.add(MovieRankSectionYear(
         year: '2017',
         type: f['type'],
@@ -46,68 +46,73 @@ class MovieRankList {
       ));
     });
 
-    list.add(MovieViewSectionHeader("2016"));
-    MovieRankList.list.forEach((f) {
-      list.add(MovieRankSectionYear(
-        year: '2016',
-        type: f['type'],
-        title: f['title'],
-        subTitle: f['subTitle'],
-      ));
-    });
+    // list.add(MovieViewSectionHeader("2016"));
+    // MovieRankList.list.forEach((f) {
+    //   list.add(MovieRankSectionYear(
+    //     year: '2016',
+    //     type: f['type'],
+    //     title: f['title'],
+    //     subTitle: f['subTitle'],
+    //   ));
+    // });
 
-    list.add(MovieViewSectionHeader("2015"));
-    MovieRankList.list.forEach((f) {
-      list.add(MovieRankSectionYear(
-        year: '2015',
-        type: f['type'],
-        title: f['title'],
-        subTitle: f['subTitle'],
-      ));
-    });
+    // list.add(MovieViewSectionHeader("2015"));
+    // MovieRankList.list.forEach((f) {
+    //   list.add(MovieRankSectionYear(
+    //     year: '2015',
+    //     type: f['type'],
+    //     title: f['title'],
+    //     subTitle: f['subTitle'],
+    //   ));
+    // });
     return list;
   }
 
-  static const list = [
+  static const list2018 = [
     {
       'type': '1',
-      'title': '评分最高华语电影',
+      'title': '评分最高的华语电影',
       'subTitle': '评分最高',
     },
     {
       'type': '2',
-      'title': '评分最高外语电影',
+      'title': '评分最高的外语电影',
       'subTitle': '评分最高',
     },
     {
-      'type': '9',
-      'title': '年度冷门佳片',
-      'subTitle': '年度电影',
+      'type': '4',
+      'title': '最受关注的院线电影',
+      'subTitle': '最受关注',
     },
     {
       'type': '5',
       'title': '最受关注的非院线电影',
-      'subTitle': '年度电影',
+      'subTitle': '最受关注',
     },
     {
       'type': '8',
-      'title': '最期待华语独立佳作',
-      'subTitle': '年度电影',
+      'title': '年度最期待华语独立佳作',
+      'subTitle': '年度最期待',
+    },
+    {
+      'type': '9',
+      'title': '年度冷门佳片',
+      'subTitle': '年度冷门',
     },
     {
       'type': '11',
       'title': '评分最高的韩国电影',
-      'subTitle': '年度电影',
+      'subTitle': '评分最高',
     },
     {
       'type': '12',
       'title': '评分最高的日本电影',
-      'subTitle': '年度电影',
+      'subTitle': '评分最高',
     },
     {
       'type': '14',
       'title': '评分最高的欧洲电影',
-      'subTitle': '年度电影',
+      'subTitle': '评分最高',
     },
     {
       'type': '15',
@@ -116,63 +121,296 @@ class MovieRankList {
     },
     {
       'type': '17',
-      'title': '评分最高的喜剧',
-      'subTitle': '年度电影',
+      'title': '评分最高的喜剧片',
+      'subTitle': '评分最高',
     },
     {
       'type': '18',
-      'title': '评分最高的爱情',
-      'subTitle': '年度电影',
+      'title': '评分最高的爱情片',
+      'subTitle': '评分最高',
     },
     {
       'type': '20',
       'title': '评分最高的科幻/动作片',
-      'subTitle': '年度电影',
+      'subTitle': '评分最高',
     },
     {
       'type': '21',
       'title': '评分最高的恐怖/惊悚片',
-      'subTitle': '年度电影',
+      'subTitle': '评分最高',
     },
     {
       'type': '23',
       'title': '评分最高的动画片',
-      'subTitle': '年度电影',
+      'subTitle': '评分最高',
     },
     {
       'type': '24',
       'title': '评分最高的纪录片',
-      'subTitle': '年度电影',
+      'subTitle': '评分最高',
     },
     {
       'type': '26',
       'title': '评分最高的短片',
-      'subTitle': '年度电影',
+      'subTitle': '评分最高',
     },
     {
       'type': '27',
       'title': '年度电影原声',
       'subTitle': '年度电影',
     },
+    // {
+    //   'type': '51',
+    //   'title': '每月最热门电影',
+    //   'subTitle': '最热门',
+    // },
     {
-      'type': '29',
-      'title': '评分最高的大陆剧集',
+      'type': '53',
+      'title': '上映10周年的电影',
+      'subTitle': '10周年',
+    },
+    {
+      'type': '54',
+      'title': '上映20周年的电影',
+      'subTitle': '20周年',
+    },
+    {
+      'type': '55',
+      'title': '上映30周年的电影',
+      'subTitle': '30周年',
+    },
+    {
+      'type': '56',
+      'title': '上映40周年的电影',
+      'subTitle': '40周年',
+    },
+    {
+      'type': '57',
+      'title': '上映50周年的电影',
+      'subTitle': '40周年',
+    },
+    {
+      'type': '63',
+      'title': '2019最值得期待的外语电影',
+      'subTitle': '最值得期待',
+    },
+    {
+      'type': '64',
+      'title': '2019最值得期待的华语电影',
+      'subTitle': '最值得期待',
+    },
+    // {
+    //   'type': '66',
+    //   'title': '2018你可能错过的好片',
+    //   'subTitle': '可能错过',
+    // },
+  ];
+
+  static const list2017 = [
+    {
+      'type': '1',
+      'title': '评分最高的外语电影',
+      'subTitle': '评分最高',
+    },
+    {
+      'type': '2',
+      'title': '评分最高的华语电影',
+      'subTitle': '评分最高',
+    },
+    {
+      'type': '4',
+      'title': '最受关注的院线电影',
+      'subTitle': '最受关注',
+    },
+    {
+      'type': '5',
+      'title': '最受关注的非院线电影',
+      'subTitle': '最受关注',
+    },
+    {
+      'type': '8',
+      'title': '年度最期待华语独立佳作',
+      'subTitle': '最期待',
+    },
+    {
+      'type': '9',
+      'title': '年度中国大陆网络/电视电影',
+      'subTitle': '网络',
+    },
+    {
+      'type': '10',
+      'title': '年度冷门佳片',
+      'subTitle': '冷门佳片',
+    },
+    {
+      'type': '12',
+      'title': '评分最高的韩国电影',
+      'subTitle': '评分最高',
+    },
+    {
+      'type': '13',
+      'title': '评分最高的日本电影',
+      'subTitle': '评分最高',
+    },
+    {
+      'type': '14',
+      'title': '评分最高的欧洲电影',
+      'subTitle': '评分最高',
+    },
+    {
+      'type': '16',
+      'title': '年度LGBT电影',
       'subTitle': '年度电影',
     },
     {
-      'type': '30',
-      'title': '评分最高的英美剧(新剧)',
-      'subTitle': '年度电影',
+      'type': '17',
+      'title': '评分最高的喜剧片',
+      'subTitle': '评分最高',
     },
     {
-      'type': '33',
-      'title': '评分最高的日剧',
-      'subTitle': '年度电影',
+      'type': '18',
+      'title': '评分最高的爱情片',
+      'subTitle': '评分最高',
     },
     {
-      'type': '36',
-      'title': '评分最高的韩剧',
-      'subTitle': '年度电影',
-    }
+      'type': '20',
+      'title': '评分最高的科幻/动作片',
+      'subTitle': '评分最高',
+    },
+    {
+      'type': '21',
+      'title': '评分最高的恐怖片',
+      'subTitle': '评分最高',
+    },
+    {
+      'type': '22',
+      'title': '评分最高的惊悚/犯罪片',
+      'subTitle': '评分最高',
+    },
+    {
+      'type': '24',
+      'title': '评分最高的动画片',
+      'subTitle': '评分最高',
+    },
+    {
+      'type': '25',
+      'title': '评分最高的纪录片',
+      'subTitle': '评分最高',
+    },
+    {
+      'type': '26',
+      'title': '评分最高的短片',
+      'subTitle': '评分最高',
+    },
+    {
+      'type': '27',
+      'title': '年度最佳电影原声',
+      'subTitle': '评分最高',
+    },
+    {
+      'type': '56',
+      'title': '1月最热门电影',
+      'subTitle': '最热门',
+    },
+    {
+      'type': '57',
+      'title': '2月最热门电影',
+      'subTitle': '最热门',
+    },
+    {
+      'type': '58',
+      'title': '3月最热门电影',
+      'subTitle': '最热门',
+    },
+    {
+      'type': '60',
+      'title': '4月最热门电影',
+      'subTitle': '最热门',
+    },
+    {
+      'type': '61',
+      'title': '5月最热门电影',
+      'subTitle': '最热门',
+    },
+    {
+      'type': '62',
+      'title': '6月最热门电影',
+      'subTitle': '最热门',
+    },
+    {
+      'type': '64',
+      'title': '7月最热门电影',
+      'subTitle': '最热门',
+    },
+    {
+      'type': '65',
+      'title': '8月最热门电影',
+      'subTitle': '最热门',
+    },
+    {
+      'type': '66',
+      'title': '9月最热门电影',
+      'subTitle': '最热门',
+    },
+    {
+      'type': '68',
+      'title': '10月最热门电影',
+      'subTitle': '最热门',
+    },
+    {
+      'type': '69',
+      'title': '11月最热门电影',
+      'subTitle': '最热门',
+    },
+    {
+      'type': '70',
+      'title': '12月最热门电影',
+      'subTitle': '最热门',
+    },
+    {
+      'type': '72',
+      'title': '上映10周年的电影',
+      'subTitle': '10周年',
+    },
+    {
+      'type': '73',
+      'title': '上映20周年的电影',
+      'subTitle': '20周年',
+    },
+    {
+      'type': '74',
+      'title': '上映30周年的电影',
+      'subTitle': '30周年',
+    },
+    {
+      'type': '75',
+      'title': '上映40周年的电影',
+      'subTitle': '40周年',
+    },
+    {
+      'type': '76',
+      'title': '上映50周年的电影',
+      'subTitle': '50周年',
+    },
+    {
+      'type': '85',
+      'title': '2018最值得期待的外语电影',
+      'subTitle': '最值得期待',
+    },
+    {
+      'type': '86',
+      'title': '2018最值得期待的华语电影',
+      'subTitle': '最值得期待',
+    },
+    {
+      'type': '87',
+      'title': '2017-2018北美颁奖季最期待获奖佳片',
+      'subTitle': '北美颁奖季',
+    },
+    // {
+    //   'type': '89',
+    //   'title': '2017你可能错过的好片',
+    //   'subTitle': '最热门',
+    // },
   ];
 }
--- lib/tv/tv_choose_page.dart
+++ lib/tv/tv_choose_page.dart
@@ -3,6 +3,8 @@ import 'package:flutter_douban2/tv/tv_choose_section.dart';
 import 'package:flutter_douban2/util/label_constant.dart';
 import 'package:flutter_douban2/util/screen_size.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:flutter_douban2/movie/movie_view_section_header.dart';
+import 'package:flutter_douban2/movie/movie_rank_section_year.dart';
 
 class TVChoosePage extends StatelessWidget {
   @override
@@ -37,6 +39,172 @@ class TVChoosePage extends StatelessWidget {
         LabelConstant.TV_CHOOSE_CHANNEL_LIST,
       ),
     ];
+
+    views.add(MovieViewSectionHeader("2018", navigatable: false));
+    list2018.forEach((f) {
+      views.add(MovieRankSectionYear(
+        year: '2018',
+        type: f['type'],
+        title: f['title'],
+        subTitle: f['subTitle'],
+      ));
+    });
+
+    views.add(MovieViewSectionHeader("2017", navigatable: false));
+    list2017.forEach((f) {
+      views.add(MovieRankSectionYear(
+        year: '2017',
+        type: f['type'],
+        title: f['title'],
+        subTitle: f['subTitle'],
+      ));
+    });
     return views;
   }
+
+  static const list2018 = [
+    {
+      'type': '29',
+      'title': '评分最高的大陆剧集',
+      'subTitle': '剧集/综艺',
+    },
+    {
+      'type': '30',
+      'title': '评分最高的英美剧(新剧)',
+      'subTitle': '评分最高',
+    },
+    {
+      'type': '31',
+      'title': '评分最高的英美剧(非新剧)',
+      'subTitle': '评分最高',
+    },
+    {
+      'type': '33',
+      'title': '评分最高的日剧',
+      'subTitle': '评分最高',
+    },
+    {
+      'type': '34',
+      'title': '评分最高的韩剧',
+      'subTitle': '评分最高',
+    },
+    {
+      'type': '36',
+      'title': '评分最高的大陆电视综艺',
+      'subTitle': '评分最高',
+    },
+    {
+      'type': '37',
+      'title': '评分最高的大陆网络综艺',
+      'subTitle': '评分最高',
+    },
+    {
+      'type': '38',
+      'title': '最受关注的韩国综艺',
+      'subTitle': '最受关注',
+    },
+    {
+      'type': '40',
+      'title': '评分最高的动画剧集',
+      'subTitle': '评分最高',
+    },
+    {
+      'type': '41',
+      'title': '评分最高的纪录剧集',
+      'subTitle': '评分最高',
+    },
+    {
+      'type': '59',
+      'title': '开播10周年的剧集',
+      'subTitle': '10周年',
+    },
+    {
+      'type': '60',
+      'title': '开播20周年的剧集',
+      'subTitle': '20周年',
+    },
+    {
+      'type': '62',
+      'title': '2019最值得期待的剧集',
+      'subTitle': '最值得期待',
+    },
+  ];
+
+  static const list2017 = [
+    {
+      'type': '29',
+      'title': '评分最高的大陆网络剧集',
+      'subTitle': '评分最高',
+    },
+    {
+      'type': '30',
+      'title': '评分最高的大陆电视剧集',
+      'subTitle': '评分最高',
+    },
+    {
+      'type': '32',
+      'title': '评分最高的英美剧(新剧)',
+      'subTitle': '评分最高',
+    },
+    {
+      'type': '33',
+      'title': '评分最高的英美剧(非新剧)',
+      'subTitle': '评分最高',
+    },
+    {
+      'type': '35',
+      'title': '评分最高的日剧',
+      'subTitle': '评分最高',
+    },
+    {
+      'type': '36',
+      'title': '评分最高的韩剧',
+      'subTitle': '评分最高',
+    },
+    {
+      'type': '37',
+      'title': '评分最高的其他地区剧集',
+      'subTitle': '评分最高',
+    },
+    {
+      'type': '39',
+      'title': '最受关注的大陆电视综艺',
+      'subTitle': '最受关注',
+    },
+    {
+      'type': '40',
+      'title': '最受关注的大陆网络综艺',
+      'subTitle': '最受关注',
+    },
+    {
+      'type': '41',
+      'title': '评分最高的动画剧集',
+      'subTitle': '评分最高',
+    },
+    {
+      'type': '43',
+      'title': '评分最高的动画剧集',
+      'subTitle': '评分最高',
+    },
+    {
+      'type': '44',
+      'title': '评分最高的纪录剧集',
+      'subTitle': '评分最高',
+    },
+    {
+      'type': '78',
+      'title': '开播10周年的剧集',
+      'subTitle': '10周年',
+    },
+    {
+      'type': '79',
+      'title': '开播20周年的剧集',
+      'subTitle': '20周年',
+    },
+    {
+      'type': '80',
+      'title': '开播30周年的剧集',
+      'subTitle': '30周年',
+    },
+  ];
 }
