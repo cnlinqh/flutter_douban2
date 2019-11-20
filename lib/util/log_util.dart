5a1035b9d8c9d619be95282dcddca4a2fd1380f3
Quan-hao.Lin <quan-hao.lin@sap.com>
2019-11-20 15:46:13 +0800

    print timestamp during log

--- lib/util/log_util.dart
+++ lib/util/log_util.dart
@@ -1,6 +1,11 @@
+import 'package:intl/intl.dart';
+
 class LogUtil {
   static bool enable = false;
+  static DateFormat format = DateFormat('hh:mm:ss SSS a');
   static log(msg) {
-    if (enable) print(msg);
+    if (enable) {
+      print('${format.format(DateTime.now())} : $msg');
+    }
   }
 }
